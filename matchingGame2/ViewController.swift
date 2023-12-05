6f34d9b82ef37413cb5d6b665996b2b9972f048c
Mykaela Wallen <mykaelawallen@gmail.com>
2023-12-05 09:40:56 -0600

    Initial Commit

---
+++ matchingGame2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  matchingGame2
+//
+//  Created by MYKAELA WALLEN on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ matchingGame2UITests/matchingGame2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  matchingGame2UITestsLaunchTests.swift
+//  matchingGame2UITests
+//
+//  Created by MYKAELA WALLEN on 12/5/23.
+//
+
+import XCTest
+
+final class matchingGame2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
